6952179d4c0742c39e6736e536c5c2c89c219710
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-16 15:18:21 +0100

    hide numbers and message, show form

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -8,3 +8,14 @@ numbers.innerHTML = `
     <li>${Math.floor(Math.random() * 50)}</li>
     <li>${Math.floor(Math.random() * 50)}</li>
 `;
+ 
+//nascondere i numeri e il messaggio dopo 30 secondi
+//mostrare il form dopo 30 secondi
+const numbImputs = document.getElementById('answers-form')
+const message = document.getElementById('instructions')
+setTimeout(function(){
+    numbers.classList.add('d-none')
+    message.classList.add('d-none')
+    numbImputs.classList.remove('d-none')
+},3000)
+
